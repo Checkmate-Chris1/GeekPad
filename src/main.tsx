ae23787e97d89120156add1771954632df8dd029
Christopher Walden <checkdoesgames@gmail.com>
2025-06-05 08:50:34 -0700

    fixed Bootstrap
    I misspelled oops

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'boostrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './pages/App.tsx'
 import NoteEditor from './pages/NoteEditor.tsx'
 
