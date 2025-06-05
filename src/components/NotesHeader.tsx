5f94ff02dcbba366e0e065596d74cd438c2e783c
Christopher Walden <checkdoesgames@gmail.com>
2025-06-04 22:28:11 -0700

    Update
    working on page

--- src/components/NotesHeader.tsx
+++ src/components/NotesHeader.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
+import './NotesHeader.css'
+
 function NotesHeader() {
+    const [title, setTitle] = useState('Lorem Ipsum');
+    const [createdDate, setCreatedDate] = useState('6/4/2025');
+    const [modifiedDate, setModifiedDate] = useState('6/4/2025');
+
     return (
-        <header className="notes-header">
-            <h1>GeekPad</h1>
-            <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/notes">Notes</a></li>
-                    <li><a href="/settings">Settings</a></li>
-                </ul>
-            </nav>
-        </header>
+        <div id="notes-header">
+            <div className="header-info">
+                <div id="metadata-info">
+                    <span className="metadata-item">Title: {title}</span>
+                    <span className="metadata-item">Created: {createdDate}</span>
+                    <span className="metadata-item">Modified: {modifiedDate}</span>
+                </div>
+                <button onClick={() => alert('Register functionality not implemented yet!')}>Login/Register</button>
+            </div>
+        </div>
     );
 }
 
