92c02960d1b42a81b1be4368ca16a63d5b732c52
Christopher Walden <checkdoesgames@gmail.com>
2025-06-09 23:22:41 -0700

    sketchout complete

--- src/components/NotesTextArea.tsx
+++ src/components/NotesTextArea.tsx
@@ -3,15 +3,7 @@ import './NotesTextArea.css';
 function NotesTextArea() {
     return (
         <div className="notes-textarea">
-            <textarea
-                className="form-control"
-                placeholder="Type your notes here..."
-                rows={20}
-                cols={75}
-                
-            ></textarea>
-            <div className="mb-3">
-</div>
+            <textarea className="form-control" placeholder="Type your notes here..." />
         </div>
         
     )
