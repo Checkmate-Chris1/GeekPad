be39a481cedf961f85bf8101e1f77bd9cb6593e6
Christopher Walden <checkdoesgames@gmail.com>
2025-06-10 22:36:40 -0700

    fixed nested markdown

--- src/components/NotesPreview.tsx
+++ src/components/NotesPreview.tsx
@@ -21,32 +21,32 @@ export default function NotesPreview({text}: Props) {
 function parseMarkdownLine(line: string) {
   if(line == '') {
     return (<br />);
-  } else if (line.startsWith('# ')) {
-    return (<h1>{line.slice(2)}</h1>);
-  } else if (line.startsWith('## ')) {
-    return (<h2>{line.slice(3)}</h2>);  
-  } else if (line.startsWith('### ')) {
-    return (<h3>{line.slice(4)}</h3>);
-  } else if (line.startsWith('#### ')) {
-    return (<h4>{line.slice(5)}</h4>);
-  } else if (line.startsWith('##### ')) {
-    return (<h5>{line.slice(6)}</h5>);
-  } else if (line.startsWith('###### ')) {
-    return (<h6>{line.slice(7)}</h6>);
-  } else if (line.match('---')) {
+    } else if (line.startsWith('# ')) {
+    return (<h1>{parseMarkdownLine(line.slice(2))}</h1>);
+    } else if (line.startsWith('## ')) {
+    return (<h2>{parseMarkdownLine(line.slice(3))}</h2>);  
+    } else if (line.startsWith('### ')) {
+    return (<h3>{parseMarkdownLine(line.slice(4))}</h3>);
+    } else if (line.startsWith('#### ')) {
+    return (<h4>{parseMarkdownLine(line.slice(5))}</h4>);
+    } else if (line.startsWith('##### ')) {
+    return (<h5>{parseMarkdownLine(line.slice(6))}</h5>);
+    } else if (line.startsWith('###### ')) {
+    return (<h6>{parseMarkdownLine(line.slice(7))}</h6>);
+    } else if (line.match('---')) {
     return (<hr />);
-  } else if( line.startsWith('> ')) {
-    return (<blockquote>{line.slice(2)}</blockquote>);
-  } else if (line.startsWith('* ') || line.startsWith('- ')) {
-    return (<ul><li>{line.slice(2)}</li></ul>);
-  } else if (line.startsWith('1. ')) {
-    return (<ol><li>{line.slice(3)}</li></ol>); // ordered list, work-in-progress
-  } else if (line.startsWith('`') && line.endsWith('`')) {
-    return (<code>{line.slice(1, -1)}</code>);
+    } else if( line.startsWith('> ')) {
+    return (<blockquote>{parseMarkdownLine(line.slice(2))}</blockquote>);
+    } else if (line.startsWith('* ') || line.startsWith('- ')) {
+    return (<ul><li>{parseMarkdownLine(line.slice(2))}</li></ul>);
+    } else if (line.startsWith('1. ')) {
+    return (<ol><li>{parseMarkdownLine(line.slice(3))}</li></ol>); // ordered list, work-in-progress
+    } else if (line.startsWith('`') && line.endsWith('`')) {
+    return (<code>{parseMarkdownLine(line.slice(1, -1))}</code>);
   } else if (line.startsWith('**') && line.endsWith('**')) {
-    return (<strong>{line.slice(2, -2)}</strong>);
+    return (<strong>{parseMarkdownLine(line.slice(2, -2))}</strong>);
   } else if (line.startsWith('*') && line.endsWith('*')) {
-    return (<p><em>{line.slice(1, -1)}</em></p>);
+    return (<p><em>{parseMarkdownLine(line.slice(1, -1))}</em></p>);
   } else {
     return (<p>{line}</p>);
   }
